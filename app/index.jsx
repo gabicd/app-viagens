4d24b95df35fbb7aae7cbe471d0aec960e80c517
gabicd <gabicd3323@gmail.com>
2025-06-09 19:12:14 -0300

    tela inicial -rascunho

--- app/index.jsx
+++ app/index.jsx
@@ -2,25 +2,59 @@ import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.view}>
-      <Image
-        style={styles.app_logo}
-        resizeMode="cover"
-        source={require('/home/gabis/projetos/rn-app/assets/images/vetor mapa.png')}
-      />
-      <TouchableOpacity style={styles.login_button}>
-        <Text style={styles.login_button_text}>Entrar com email</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <View style={styles.div_logo}>
+        <Image
+            style={styles.app_logo}
+            resizeMode="cover"
+            source={require('/home/gabis/projetos/rn-app/assets/images/vetor mapa.png')}/>
+      
+        <TouchableOpacity style={styles.login_button}>
+          <Text style={styles.login_button_text}>Entrar com email</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.signup_button}>
+          <Text style={styles.login_button_text}>Cadastre-se</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.option_text}>Ou então, continue com uma rede social</Text>
+      </View>
+      <View style={styles.div_icons}>
+        
+        <Image
+            style={styles.social_icon}
+            source={require('/home/gabis/projetos/rn-app/assets/images/facebook.png')}/>
+
+        <Image
+            style={styles.social_icon}
+            source={require('/home/gabis/projetos/rn-app/assets/images/google.png')}/>
+
+
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  view: {
+  container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
+
+  div_logo: {
+    flex: 3,
+  },
+
+  div_icons: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    marginTop: 0,
+  },
+
   app_logo: {
     width: 400,
     height: 400,
@@ -31,11 +65,37 @@ const styles = StyleSheet.create({
     paddingVertical: 16,
     paddingHorizontal: 32,
     marginTop: 24,
+    alignSelf: 'center',
   },
+
+  signup_button: {
+    backgroundColor: '#7678ED',
+    borderRadius: 8,
+    paddingVertical: 16,
+    paddingHorizontal: 48,
+    marginTop: 24,
+    alignSelf: 'center',
+  },
+
   login_button_text: {
     color: '#fff',
     fontSize: 16,
     fontWeight: 'bold',
     textAlign: 'center',
   },
+
+  option_text: {
+    color: '#B2B2B2',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 24,
+  },
+
+  social_icon: {
+    width: 40,
+    height: 40,
+    marginHorizontal: 10,
+  },
+
 });
